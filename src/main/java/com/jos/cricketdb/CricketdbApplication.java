e81e0a47ad6e70b921f7d2ff2f4ec188d00a9ad9
Joe Offenberg <joe@sweagle.com>
2019-10-18 12:39:29 -0400

    init

---
+++ src/main/java/com/jos/cricketdb/CricketdbApplication.java
@@ -0,0 +1,13 @@
+package com.jos.cricketdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketdbApplication.class, args);
+	}
+
+}
